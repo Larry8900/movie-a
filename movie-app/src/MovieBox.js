2aa91b8de409b9a4d47b14fb59902787caa14487
larry8900 <quickexchange488@gmail.com>
2023-09-15 12:19:42 +0100

    updated

--- movie-app/src/MovieBox.js
+++ movie-app/src/MovieBox.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import {Modal, show} from 'react-bootstrap';
+import React, {useState} from 'react';
 const API_IMG ="https://image.tmdb.org/t/p/original/"
 
 const MovieBox =({title,poster_path, vote_average, release_date, overview })=>{
+   const [show, setShow]=useState(false);
+
+   const handleShow=()=>setShow(true);
+   const handleClose=()=>setShow(false);
+   
+   
+   
     return(
         <div className='card text-center bg-secondary mb-3'> 
             <div className='card-body'>
                 <img className='card-img-top' src={API_IMG+poster_path} alt=''></img>
                 <div className='card-body'>
-                    <button type='button' className='btn btn-dark'>View more</button>
+                <span>USA, 2021</span>
+                <h4>{title}</h4>
+                <h5>IMDb {vote_average}</h5> 
+                    <button type='button' className='btn btn-dark' onClick={handleShow}>View more</button>
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title></Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                        <img className='card-img-top' src={API_IMG+poster_path} alt=''></img>
+                        <h3>{title}</h3>
+                        <h5>Overview</h5>
+                        <p>{overview}</p>
+                        
+
+                        </Modal.Body>
+                    </Modal>
                 </div>
             </div>
-            {/* <h1>{title}</h1>
+            {/* 
             <img src={API_IMG+poster_path} alt=""></img>
             <p>{overview}</p> */}
         </div>
