f161b29278e888340eb559bd4b0d16faa19468b8
larry8900 <quickexchange488@gmail.com>
2023-09-16 11:54:59 +0100

    updated

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -2,14 +2,15 @@ import React,{useState,useEffect} from 'react';
 import './App.css';
 import MovieBox from './MovieBox'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, Container, Nav} from 'react-bootstrap';
+import {Navbar, Container, Nav,Form, FormControl, Button} from 'react-bootstrap';
 
 
 const API_URL="https://api.themoviedb.org/3/movie/top_rated?api_key=414eec00dd3b35b52e5201e1ec693543"
-
+const API_SEARCH ="https://api.themoviedb.org/3/search/movie?api_key=414eec00dd3b35b52e5201e1ec693543&query";
 function App() {
 
   const [movies, setMovies]=useState([]);
+  const [query, setQuerry]=useState('');
   
   useEffect(()=>{
     fetch(API_URL)
@@ -20,31 +21,66 @@ function App() {
     })
   },[])
 
+  const searchMovie = async(e)=>{
+    e.preventDefault();
+    console.log("searching");
+    try{
+      const url=`https://api.themoviedb.org/3/search/movie?api_key=414eec00dd3b35b52e5201e1ec693543&query=${ query }`;
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      setMovies(data.results);
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
+  const changeHandler=(e)=>{
+    setQuerry(e.target.value);
+  }
   return (
     <>
-    <Navbar bg="dark" expand="lg" variant="dark">
+    <Navbar bg="dark" className='Navbar' expand="lg" variant="dark">
       <Container fluid>
-        <Navbar.Brand href="">MovieBox</Navbar.Brand>
-        <Navbar.Brand href="">Trending</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll">
+        <Navbar.Brand href="/home">MovieBox</Navbar.Brand>
+        
+        <Navbar.Toggle aria-controls="navbarScroll"></Navbar.Toggle>
 
           <Navbar.Collapse id='navbarScrool'>
             <Nav
             className="me-auto my-2 my-lg-3"
             style={{maxHeight: '100px'}}
-            NavbarScroll></Nav>
+            NavbarScroll>
 
+            </Nav>
+
+            <Form className="d-flex" onSubmit={searchMovie}>
+              <FormControl 
+              type='search'
+              placeholder='What do you want to watch?'
+              className='me-2'
+              aria-label='search'
+              name="query"
+              value={query} onChange={changeHandler}></FormControl>
+              <Button variant="secondary" type='submit'>Search</Button>
+            </Form>
           </Navbar.Collapse>
-        </Navbar.Toggle>
       </Container>
     </Navbar>
+    <div>
+    <h3>Featured Movie</h3>
+    {movies.length > 0 ?(
       <div className='container'>
         <div className='grid'>
           {movies.map((moviesReq)=>
           <MovieBox key={moviesReq.id} {...moviesReq} />)}
         </div>
       </div>
-    </>
+    ):(
+      <h2>Sorry !! No Movies Found</h2>
+    )}
+    </div> 
+    </>    
  
   );
 }
--- movie-app/src/MovieBox.js
+++ movie-app/src/MovieBox.js
@@ -1,8 +1,9 @@
 import {Button, Modal, show} from 'react-bootstrap';
 import React, {useState} from 'react';
+
 const API_IMG ="https://image.tmdb.org/t/p/original/"
 
-const MovieBox =({title,poster_path, vote_average, release_date, overview })=>{
+const MovieBox =({title,poster_path,id, vote_average, release_date, overview })=>{
    const [show, setShow]=useState(false);
 
    const handleShow=()=>setShow(true);
@@ -11,13 +12,14 @@ const MovieBox =({title,poster_path, vote_average, release_date, overview })=>{
    
    
     return(
-        <div className='card text-center bg-secondary mb-3'> 
-            <div className='card-body'>
-                <img className='card-img-top' style={{width: '14rem'}} src={API_IMG+poster_path} alt=''></img>
+        <div className='card text-center bg-white mb-3'> 
+            <div className='card-body'  data-testid="movie-card">
+                <img data-testid="movie-poster" className='card-img-top' src={API_IMG+poster_path} alt=''></img>
                 <div className='card-body'>
-                <span>USA, 2021</span>
-                <h4>{title}</h4>
-                <h5>IMDb {vote_average}</h5> 
+                <span data-testid="movie-release-date">{release_date}</span>
+                <h4 data-testid="movie-title">{title}</h4>
+                <h5 > IMDb </h5> 
+                <p data-testid="movie-overview" >{overview}</p>
                     <button type='button' className='btn btn-dark' onClick={handleShow}>View more</button>
                     <Modal show={show} onHide={handleClose}>
                         <Modal.Header closeButton>
